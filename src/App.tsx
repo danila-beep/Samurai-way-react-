a6e100aeae3f8d3e017d30e0128fc427e88a805a
danila-beep <danil123w29@gmail.com>
2023-02-21 20:29:55 +0300

    changed App.css & App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home </a>
+            <a href="#">News Feed </a>
+            <a href="#">Messages</a>
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <div>Technologies</div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
